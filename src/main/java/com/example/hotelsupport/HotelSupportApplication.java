f86be653c066869bba05eb2215f275f18e43822f
Madi <madizhaksigeldi@gmail.com>
2020-11-06 23:25:37 +0600

    Hystrix + Bulkhead

--- src/main/java/com/example/hotelsupport/HotelSupportApplication.java
+++ src/main/java/com/example/hotelsupport/HotelSupportApplication.java
@@ -2,10 +2,16 @@ package com.example.hotelsupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class HotelSupportApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelSupportApplication.class, args);
--- src/main/java/com/example/hotelsupport/controller/SupportController.java
+++ src/main/java/com/example/hotelsupport/controller/SupportController.java
@@ -3,11 +3,14 @@ package com.example.hotelsupport.controller;
 import com.example.hotelsupport.model.CaseSupport;
 import com.example.hotelsupport.service.CaseService;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,13 +19,25 @@ public class SupportController {
     @Autowired
     CaseService service;
 
+    @GetMapping(path = "/cases")
+    public List<CaseSupport> getAllCases() {
+        return service.getAllCases();
+    }
+
+    @HystrixCommand(fallbackMethod = "getFallbackService",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10"),
+            }
+    )
     @GetMapping(path = "/cases/{id}")
     public Optional<CaseSupport> getCaseById(@PathVariable("id") long id) {
         return service.getCaseById(id);
     }
 
-    @GetMapping(path = "/cases")
-    public List<CaseSupport> getAllCases() {
-        return service.getAllCases();
+    public List<CaseSupport> getFallbackService(@PathVariable("id") long id) {
+        List<CaseSupport> supportList = new ArrayList<>();
+        supportList.add(new CaseSupport());
+        return supportList;
     }
 }
